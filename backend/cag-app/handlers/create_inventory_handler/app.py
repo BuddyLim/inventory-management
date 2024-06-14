5e4f78ad18860ce07b2a236699fb8f5118e4367e
Kuang Tar Lim <buddy.tlimk@gmail.com>
2024-06-14 10:58:40 +0800

    [Add] Initial create inventory flow

--- backend/cag-app/handlers/create_inventory_handler/app.py
+++ backend/cag-app/handlers/create_inventory_handler/app.py
@@ -1,64 +1,64 @@
 import json
 import boto3
 import os
-import uuid
+from typing import Dict
+from hashlib import sha1
 from decimal import Decimal
 from datetime import datetime
+from boto3.dynamodb.conditions import Key
+from mypy_boto3_dynamodb import ServiceResource
 
 
 TABLE_NAME = os.environ['TABLE_NAME']
 ENDPOINT_OVERRIDE = os.environ['ENDPOINT_OVERRIDE']
 
 
-dynamodb = boto3.resource('dynamodb', endpoint_url=ENDPOINT_OVERRIDE)
+dynamodb: ServiceResource = boto3.resource('dynamodb', endpoint_url=ENDPOINT_OVERRIDE)
 table = dynamodb.Table(TABLE_NAME)
 
+
+def return_sha1_hash(text: str) -> str:
+    return sha1(text.encode('utf-8')).hexdigest()
+
+
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    evt_val = json.loads(event['body'])
+    
+    name = evt_val['name']
+    price = evt_val['price']
+    category = evt_val['category']
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-    id = str(uuid.uuid4())
+    id = return_sha1_hash(text=name)
+    
     last_updated_dt = datetime.now() 
     last_updated_str = last_updated_dt.strftime("%m/%d/%Y %H:%M:%S")
+
     print("---")
+    print("Event: ", event)
     print("Table name: ", TABLE_NAME)
     print("ID: ", id)
     print("last_updated_dt: ", last_updated_str)
     print("---")
 
-    table.put_item(
-        Item={
-            'id': id,
-            "name": "Notebook", 
-            "category": "Stationary", 
-            "price": Decimal(5.5),
-            "last_updated_dt": last_updated_str 
-        }
+    resp = table.update_item(
+        Key={
+            'id': id
+        },
+        UpdateExpression='SET #price= :price, #last_updated_dt= :last_updated_dt, #category = :category, #name = :name',
+        ConditionExpression='attribute_exists(id) OR attribute_not_exists (id)',
+        ExpressionAttributeValues={
+            ':price':  Decimal(price),
+            ':last_updated_dt': last_updated_str,
+            ":category": category,
+            ":name": name
+        },
+        ExpressionAttributeNames={
+            '#price': 'price',
+            '#last_updated_dt': 'last_updated_dt',
+            '#category': 'category',
+            '#name': 'name',
+        },
+        ReturnValues='ALL_NEW'
     )
 
     return {
@@ -66,6 +66,5 @@ def lambda_handler(event, context):
         "body": json.dumps({
             "id": id,
             "last_updated_dt": last_updated_str
-            # "location": ip.text.replace("\n", "")
         }),
     }
