0e52e0667167afaf61aaa983fce3b1655560dcdd
Kuang Tar Lim <buddy.tlimk@gmail.com>
2024-06-20 20:48:31 +0800

    [Add] Query from backend for Inventory data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './reset.css'
 import './App.css'
 import InventoryForm from './components/inventoryForm'
-
+ 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
      <InventoryForm/>
--- frontend/src/components/inventoryForm/index.tsx
+++ frontend/src/components/inventoryForm/index.tsx
@@ -1,17 +1,54 @@
-import React, { useState } from 'react';
-import { Button, Form, Input } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { Button, Form, FormProps, Input } from 'antd';
 import axios from 'axios';
+
+enum CategoryEnum{
+  STATIONARY = "Stationary",
+  BOOKS = "BOOKS",
+  CLOTHING = "CLOTHING"
+}
+
+interface InventoryI{
+  id: string
+  name: string
+  price: number
+  category: CategoryEnum
+  last_updated_dt: string
+}
 
 const InventoryForm: React.FC = () => {
   const [form] = Form.useForm();
+  const [inventoryList, setInventoryList] = useState<InventoryI[]>([])
 
-  const handleFinish = (values) =>{
-    console.log(values)
-    axios.post("https://2uhvzh44vg.execute-api.us-east-1.amazonaws.com/Prod/inventories", {
+  useEffect(() =>{
+    axios.get(`${import.meta.env.VITE_API_BASE_URL}/inventories`)
+    .then((res) =>{
+      const { items } = res.data
+      
+      setInventoryList(items)
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }, [])
+
+  const handleFinish: FormProps<InventoryI>['onFinish'] = (values) =>{
+    axios.post(`${import.meta.env.VITE_API_BASE_URL}/inventories`, {
       ...values
     })
     .then((res) =>{
-      console.log(res)
+      const { id, last_updated_dt } = res.data
+      
+      setInventoryList(prevList =>{
+        const inventoryIndex = prevList.findIndex(inventoryIndex => inventoryIndex.id === id)
+        if(inventoryIndex === -1){
+          return [...prevList, {...values, id, last_updated_dt }]
+        }
+
+        prevList[inventoryIndex] = { ...values, id, last_updated_dt }
+
+        return [...prevList]
+      })
     })
     .catch((error) => {
       console.log(error);
@@ -20,25 +57,37 @@ const InventoryForm: React.FC = () => {
   }
 
   return (
-    <Form
-      layout='vertical'
-      form={form}
-      onFinish={handleFinish}
-      style={{ maxWidth: 600 }}
-    >
-      <Form.Item label="Name" name="name">
-        <Input />
-      </Form.Item>
-      <Form.Item label="Price" name="price">
-        <Input type='number' /> 
-      </Form.Item>
-      <Form.Item label="Category" name="category">
-        <Input />
-      </Form.Item>
-      <Form.Item>
-        <Button type="primary" htmlType="submit">Submit</Button>
-      </Form.Item>
-    </Form>
+    <>
+      <Form
+        layout='vertical'
+        form={form}
+        onFinish={handleFinish}
+        style={{ maxWidth: 600 }}
+      >
+        <Form.Item label="Name" name="name">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Price" name="price">
+          <Input type='number' /> 
+        </Form.Item>
+        <Form.Item label="Category" name="category">
+          <Input />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">Save Item</Button>
+        </Form.Item>
+      </Form>
+      <div style={{ color:"black"}}>
+        {inventoryList.map(inventory =>{
+          return(
+            <div key={inventory.id}>
+              {inventory.name} - ${inventory.price}
+            </div>
+          )
+        })}  
+      </div> 
+    </>
+   
   );
 };
 
