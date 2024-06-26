35c4475d84d3d22fb953f8005a21d0c7d70b31c0
Kuang Tar Lim <buddy.tlimk@gmail.com>
2024-06-26 22:45:55 +0800

    [Add] Responsiveness and prettify UI

--- frontend/src/components/inventoryForm/index.tsx
+++ frontend/src/components/inventoryForm/index.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { Button, Form, FormProps, Input } from "antd";
+import { Button, Form, FormProps, Input, List, Select } from "antd";
 import axios from "axios";
+import { v4 as uuid } from "uuid";
+
+import styles from "./index.module.css";
 
 enum CategoryEnum {
   STATIONARY = "Stationary",
@@ -33,31 +36,48 @@ const InventoryForm: React.FC = () => {
       });
   }, []);
 
+  const handleUpdateInventoryList = (
+    values: InventoryI,
+    id?: string,
+    last_updated_dt?: string,
+  ) => {
+    setInventoryList((prevList) => {
+      const inventoryIndex = prevList.findIndex(
+        (inventory) => inventory.name === values.name,
+      );
+      if (inventoryIndex === -1) {
+        const tempID = uuid();
+        return [...prevList, { ...values, id: tempID, last_updated_dt: "" }];
+      }
+
+      const inventory = prevList[inventoryIndex];
+
+      prevList[inventoryIndex] = {
+        ...values,
+        id: id ?? inventory.id,
+        last_updated_dt: last_updated_dt ?? inventory.last_updated_dt,
+      };
+
+      return [...prevList];
+    });
+  };
+
   const handleFinish: FormProps<InventoryI>["onFinish"] = (values) => {
+    handleUpdateInventoryList(values);
+
     axios
       .post(`${import.meta.env.VITE_API_BASE_URL}/inventories`, {
         ...values,
       })
       .then((res) => {
         const { id, last_updated_dt } = res.data;
-
-        setInventoryList((prevList) => {
-          const inventoryIndex = prevList.findIndex(
-            (inventoryIndex) => inventoryIndex.id === id,
-          );
-          if (inventoryIndex === -1) {
-            return [...prevList, { ...values, id, last_updated_dt }];
-          }
-
-          prevList[inventoryIndex] = { ...values, id, last_updated_dt };
-
-          return [...prevList];
-        });
+        handleUpdateInventoryList(values, id, last_updated_dt);
       })
       .catch((error) => {
         console.log(error);
       });
   };
+
   const handleDeleteInventory = (id: string, index: number) => {
     const tempInventory = { ...inventoryList[index] };
 
@@ -81,14 +101,9 @@ const InventoryForm: React.FC = () => {
   };
 
   return (
-    <>
-      <h3>Inventory Management</h3>
-      <Form
-        layout="vertical"
-        form={form}
-        onFinish={handleFinish}
-        style={{ maxWidth: 600 }}
-      >
+    <div className={styles["inventory_container"]}>
+      <h3 style={{ margin: "2% 0 5% 0" }}>Inventory Management</h3>
+      <Form layout="vertical" form={form} onFinish={handleFinish}>
         <Form.Item label="Name" name="name">
           <Input />
         </Form.Item>
@@ -96,31 +111,60 @@ const InventoryForm: React.FC = () => {
           <Input type="number" />
         </Form.Item>
         <Form.Item label="Category" name="category">
-          <Input />
+          <Select>
+            <Select.Option value="Clothing">Clothing</Select.Option>
+            <Select.Option value="Stationary">Stationary</Select.Option>
+            <Select.Option value="Books">Books</Select.Option>
+          </Select>
         </Form.Item>
-        <Form.Item>
-          <Button type="primary" htmlType="submit">
+        <Form.Item style={{ width: "100%" }}>
+          <Button type="primary" htmlType="submit" style={{ width: "100%" }}>
             Save Item
           </Button>
         </Form.Item>
       </Form>
-      <div style={{ color: "black" }}>
-        {inventoryList.map((inventory, index) => {
-          return (
-            <div key={inventory.id}>
-              {inventory.name} - ${inventory.price}
-              <Button
-                danger
-                type="primary"
-                onClick={() => handleDeleteInventory(inventory.id, index)}
+      <List
+        itemLayout="horizontal"
+        dataSource={inventoryList}
+        renderItem={(item, index) => (
+          <List.Item
+            actions={[
+              <a
+                key="list-delete"
+                style={{ color: "red" }}
+                onClick={() => handleDeleteInventory(item.id, index)}
               >
-                Delete
-              </Button>
-            </div>
-          );
-        })}
-      </div>
-    </>
+                delete
+              </a>,
+            ]}
+          >
+            <List.Item.Meta
+              title={item.name}
+              description={`$ ${item.price} - ${item.category}`}
+            />
+          </List.Item>
+        )}
+      />
+
+      {/* <li style={{ color: "black" }} className={styles["inventory_list"]}> */}
+      {/*     {inventoryList.map((inventory, index) => { */}
+      {/*       return ( */}
+      {/*         <ul key={inventory.id} className={styles["inventory_item"]}> */}
+      {/*           <span className={styles["inventory_name"]}> */}
+      {/*             {inventory.name} - ${inventory.price} */}
+      {/*           </span> */}
+      {/*           <Button */}
+      {/*             danger */}
+      {/*             type="primary" */}
+      {/*             onClick={() => handleDeleteInventory(inventory.id, index)} */}
+      {/*           > */}
+      {/*             Delete */}
+      {/*           </Button> */}
+      {/*         </ul> */}
+      {/*       ); */}
+      {/*     })} */}
+      {/*   </li> */}
+    </div>
   );
 };
 
